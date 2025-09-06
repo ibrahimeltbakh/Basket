00d559311731ddab00dfc83c736ca7c6466eba2e
abduallah <162515003+abuoseed@users.noreply.github.com>
2025-09-06 13:39:43 +0300

    create new branch

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import { Link } from "react-router";
 import { v4 as uuidv4 } from "uuid";
 
@@ -9,29 +9,31 @@ export default function Navbar() {
   ];
 
   return (
-    <header className="flex justify-between px-20 py-4 bg-green-900 text-gray-300 mb-8">
-      <section>
-        <Link className="hover:text-white rounded-md px-3 py-2 font-medium">
-          Logo
-        </Link>
-      </section>
-      <nav>
-        {myPages.map((page) => (
-          <Link
-            className="hover:text-white rounded-md px-3 py-2 font-medium"
-            to={page.path}
-            key={uuidv4()}>
-            {page.name}
-          </Link>
-        ))}
-      </nav>
-      <section>
-        <Link
-          className="hover:text-white rounded-md px-3 py-2 font-medium"
-          to={"cart"}>
-          Cart - 2
-        </Link>
-      </section>
+    <header className="flex justify-between">
+    <div className='w-full'>
+      <div className="bg-[#35AFA0] w-[100%] flex items-center justify-center h-10 text-white ">
+        <p className='my-auto'>Due to current circumstances, there may be slight delays in order processing</p>
+      </div>
+      <div className="flex justify-center ">
+        <div className="flex h-10 items-center justify-between">
+          <Link className="mr-3" to='/about'>About Us</Link>
+          <Link className="mr-3" to='/about'>Compare</Link>
+          <Link className="mr-3" to='/about'>Wishlist</Link>
+          <p className="mr-3">100% Secure delivery without contacting the courier</p>
+          <hr className="mr-3" />
+          <p className="mr-3">Need help? Call Us: + 0020 500</p>
+        </div>
+        <hr className="mr-3" />
+        <div className="flex items-center justify-between w-[8%]">
+          <p>English</p>
+          <p>UCD</p>
+        </div>
+       
+      </div>
+       <hr />
+    </div>  
+
+
     </header>
   );
 }
